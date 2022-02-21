d4ca1c6b68fd9a9e83b4b0d98fb78313f38297d1
noam shamgar <noamshpam12@gmail.com>
2022-02-21 02:20:29 +0200

    bug fixes

--- config/configDB.js
+++ config/configDB.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://noam:7548963@cinemacluster.vnjed.mongodb.net/cinemaDB?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://noam:123456@cinemacluster.vnjed.mongodb.net/cinemaDB?retryWrites=true&w=majority",
     ()=>{
         console.log("\x1b[32m","   successfully connected to employees database");
         require("../helpers/sysadmin"); // adding a sysadmin in case not exist
